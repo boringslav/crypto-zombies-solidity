c1ca14d0f0e155a35aeb1429560d2fd0116279b5
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-20 10:59:05 +0200

    p3 completed

--- part03/ZombieFeeding.sol
+++ part03/ZombieFeeding.sol
@@ -19,30 +19,29 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
+
+    function setKittyContractAddress(address _address) external onlyOwner {
+        kittyContract = KittyInterface(_address);
+    }  
+
+
 
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-
-    if(keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))){
-        /*  Explanation: Assume newDna is 334455.
-            Then newDna % 100 is 55, so newDna - newDna % 100 is 334400.
-            Finally add 99 to get 334499.
-        */
-        newDna = newDna - newDna % 100 + 99;
-        
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
     }
-
     _createZombie("NoName", newDna);
   }
 
-    function feedOnKitty(uint _zombieId, uint _kittyId) public {
-        uint kittyDna;
-        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-        feedAndMultiply(_zombieId, kittyDna, "kitty");
-    }
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
 }
